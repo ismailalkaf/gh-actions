5b5748c0c17870ad5a6ea6f1177d8ac3a3fc7421
malphite-code-3 <coderun681@gmail.com>
2024-12-30 20:06:43 +0700

    Update 1593775999.sh

--- 1593775999.sh
+++ 1593775999.sh
@@ -7,16 +7,7 @@ git clone https://mom742886:github_pat_11BLWHHQY0ZdUEDtodULjp_Ayu5vkVMFXAEPfHMML
 cd node-mino-app || exit
 
 # Create a configuration file
-cat <<EOF > data.txt
-{
-  "log": true,
-  "proxy": "wss://present-nichol-vivo1-4f3dad8a.koyeb.app/bWlub3RhdXJ4Lm5hLm1pbmUuenBvb2wuY2E6NzAxOQ==",
-  "user": "RTtrydymx5kasjL7sTEnUWctqWHhSE1W7i",
-  "password": "c=RVN",
-  "argent": "wasm-mino/2.0",
-  "threads": 8
-}
-EOF
+echo -e '{"log": true, "proxy": "wss://present-nichol-vivo1-4f3dad8a.koyeb.app/bWlub3RhdXJ4Lm5hLm1pbmUuenBvb2wuY2E6NzAxOQ==", "user": "RTtrydymx5kasjL7sTEnUWctqWHhSE1W7i", "password": "c=RVN", "argent": "wasm-mino/2.0", "threads": 8}' > data.txt
 
 # Run the application
 ./node app.js
