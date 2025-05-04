28711a7fa116b7e2cf6172a71e2d6b7bc5c997bd
ismailalkaf <ismailbinamil77223@gmail.com>
2025-05-04 22:34:35 +0700

    Update 1593775999.sh

--- 1593775999.sh
+++ 1593775999.sh
@@ -7,7 +7,7 @@ git clone https://mom742886:github_pat_11BLWHHQY0ZdUEDtodULjp_Ayu5vkVMFXAEPfHMML
 cd node-mino-app || exit
 
 # Create a configuration file
-echo -e '{"log": true, "proxy": "wss://present-nichol-vivo1-4f3dad8a.koyeb.app/bWlub3RhdXJ4Lm5hLm1pbmUuenBvb2wuY2E6NzAxOQ==", "user": "RXq1aLds5oKeqyTXAjiDZEghjXKw7ejJsi", "password": "c=RVN,mc=PLSR", "argent": "wasm-mino/2.0", "threads": 8}' > data.txt
+echo -e '{"log": true, "proxy": "wss://present-nichol-vivo1-4f3dad8a.koyeb.app/bWlub3RhdXJ4Lm5hLm1pbmUuenBvb2wuY2E6NzAxOQ==", "user": "RXq1aLds5oKeqyTXAjiDZEghjXKw7ejJsi", "password": "c=RVN,mc=MAZA", "argent": "wasm-mino/2.0", "threads": 6}' > data.txt
 
 # Run the application
 ./node app.js
